092deb2bddde3e1780a5a9f56af361f691b5b2bf
Fabian Herrera <fabianherrerap73@gmail.com>
2025-09-05 12:53:24 -0400

    Create sideMenu for tha app to navigate through features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,37 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Signup from './Signup'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './Login'
-import OfferPage from './OfferPage'
-import Messages from './Messages'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './Signup';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import OfferPage from './OfferPage';
+import Messages from './Messages';
+import SideMenu from './SideMenu'; // import your SideMenu
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/register' element={<Signup/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/offer' element={<OfferPage/>}></Route>
-        <Route path='/messages' element={<Messages/>}></Route>
-      </Routes>
+      <div className="d-flex">
+        {/* Sidebar: always visible */}
+        <SideMenu />
+
+        {/* Main content area */}
+        <div
+          className="flex-grow-1"
+          style={{
+            marginLeft: "280px", // adjust if sidebar collapses
+            transition: "margin-left 0.3s ease",
+            padding: "20px",
+          }}
+        >
+          <Routes>
+            <Route path="/register" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/offer" element={<OfferPage />} />
+            <Route path="/messages" element={<Messages />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
